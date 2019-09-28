1a0f620a3ca784ced3eb62441929dfeed4dfd3dc
KerwinJhong <sh940718@gmail.com>
2019-09-28 23:11:31 +0800

    seeder add Async/await

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,23 +1,15 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const Record = require('../record')
 const User = require('../user')
 const record = require('./recods.json')
+const userList = require('./users.json')
 const bcrypt = require('bcryptjs')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_xt8qncz9:98df8d4g8ftnao4jabvnqo8es5@ds157818.mlab.com:57818/heroku_xt8qncz9', { useNewUrlParser: true, useCreateIndex: true })
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/record"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true })
 
 const db = mongoose.connection
-const userList = [{
-        name: 'mom',
-        email: 'mom@example.com',
-        password: '12345678'
-    },
-    {
-        name: 'dad',
-        email: 'dad@example.com',
-        password: '12345678'
-    }
-]
 
 db.on('error', () => {
     console.log('db error')
@@ -25,36 +17,54 @@ db.on('error', () => {
 
 db.once('open', () => {
     console.log('db connected!')
-    var i = 0
-    userList.forEach(user => {
-        const newUser = new User({
-            name: user.name,
-            email: user.email,
-            password: user.password
-        })
-
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err
-                newUser.password = hash
-
-                newUser.save().then(users => {
-                    for (var j = 0; j < record.length / 2; j++) {
-                        Record.create({
-                            userId: users._id,
-                            name: record[i].name,
-                            category: record[i].category,
-                            merchant: record[i].merchant,
-                            date: record[i].date,
-                            amount: record[i].amount
-                        })
-                        i++
-                    }
-                }).catch(err => {
-                    console.log(err)
+    let createUsers = (newUser) => {
+        return new Promise((resolve, reject) => {
+            bcrypt.genSalt(10, (err, salt) =>
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) return reject(err)
+                    newUser.password = hash
+                    newUser.save()
+                    return resolve(newUser)
                 })
-            })
+            )
         })
-    })
-    console.log('done')
+    }
+
+    let createRecords = (user, person) => {
+        return new Promise((resolve, reject) => {
+            return resolve(Record.create({
+                userId: user._id,
+                name: record[person].name,
+                category: record[person].category,
+                merchant: record[person].merchant,
+                date: record[person].date,
+                amount: record[person].amount
+            }))
+        })
+    }
+
+    async function start() {
+        try {
+            let person = 0
+            for (var i = 0; i < userList.length; i++) {
+                const newUser = new User({
+                    name: userList[i].name,
+                    email: userList[i].email,
+                    password: userList[i].password
+                })
+                let user = await createUsers(newUser)
+                for (var j = 0; j < record.length / 2; j++) {
+                    await createRecords(user, person)
+                    person++
+                }
+
+            }
+            process.exit()
+        } catch (e) {
+            console.warn(e)
+        }
+    }
+
+    start()
+
 })
